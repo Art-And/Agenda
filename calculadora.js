71099d2ecaf5da4da413fbd3d393216fb6417444
Dario <rasec.hiphop20@gmail.com>
2021-05-25 00:10:58 -0500

    se añade la funcion que divide los totales

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+//aqui es donde vamos a implementar el proyecto
+
+console.log('Holis soy lizzzzz owo');
+
+console.log('Hola soy andres owo');
+
+console.log('Hola soy axell owo');
+
+console.log('Hola, soy dario owo');
+
+const calcular = (a, b)=> {
+
+    let suma = a+b;
+    let rest = a-b;
+    let mult = a*b;
+    let div = a/b;
+
+    division(suma, rest, mult, div)
+}
+function division(a, b, c, d) {
+        let resultadoD = a / b / c / d;
+        console.log(resultadoD)
+    }
